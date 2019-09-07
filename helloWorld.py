ab9db47ce5442651728e4e951cc23188dfe491d8
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2019-09-07 08:53:16 -0400

    Init to comit it

---
+++ helloWorld.py
@@ -0,0 +1,4 @@
+import os # Import library os, this will allow us to interact with windows bash commands
+
+print("Hello World") # Print text onto the console, the text inside the ("") Will be printed
+os.system("pause") # Send the pause command to windows, pausing the script before you exit
