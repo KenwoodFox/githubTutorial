e926c25ed47078fd1655a8bf71a9d3a6f5ccd472
Crazy Uncle <github@hamsrc.com>
2019-09-08 15:25:02 -0400

    Update helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,5 +1,5 @@
 import os # Import library os, this will allow us to interact with windows bash commands
 
 print("Hello World") # Print text onto the console, the text inside the ("") Will be printed
-print("Cool Beans")
+print("Cool Beans, because hot beans ain't bueno")
 os.system("pause") # Send the pause command to windows, pausing the script before you exit
